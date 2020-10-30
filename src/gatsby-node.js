53dd4a69cffb72909f4921432f1199739a37ceee
bodyno <az8321550@gmail.com>
2020-10-30 17:49:03 +0800

    feat: Support Jieba

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,38 +1,6 @@
-const natural = require('natural');
-const TfIdf = natural.TfIdf;
+var nodejieba = require('nodejieba');
 
-const blacklistKeywords = (keywords, blacklist) => {
-    if (typeof blacklist === 'function') {
-        return keywords.filter(blacklist);
-    }
-
-    // Make all blacklisted terms lowercase
-    let blacklisted = [...blacklist].map(term => term.toLowerCase());
-
-    // Then compare with actual keywords as lowercase
-    return keywords.filter(term => !blacklisted.includes(term.toLowerCase()));
-};
-
-const getKeywords = ({ text, max, blacklist }) => {
-    let textKeywords = [];
-
-    const tfidf = new TfIdf();
-    tfidf.addDocument(text);
-
-    tfidf.listTerms(0).forEach(item => textKeywords.push(item.term));
-
-    if (blacklist) {
-        textKeywords = blacklistKeywords(textKeywords, blacklist);
-    }
-
-    if (max) {
-        textKeywords = textKeywords.slice(0, max);
-    }
-
-    return textKeywords;
-};
-
-exports.onCreateNode = ({ node, actions }, { max, blacklist } = {}) => {
+exports.onCreateNode = ({ node, actions }, { max } = {}) => {
     const { createNodeField } = actions;
 
     if (
@@ -44,11 +12,9 @@ exports.onCreateNode = ({ node, actions }, { max, blacklist } = {}) => {
                 ? node.rawMarkdownBody
                 : node.rawBody;
 
-        const keywords = getKeywords({
-            text: bodyText,
-            max,
-            blacklist,
-        });
+        const keywords = nodejieba
+            .extract(bodyText, max)
+            .map(item => item.word);
 
         createNodeField({
             node,
